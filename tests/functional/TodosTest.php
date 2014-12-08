f5deb83c42b013b57b8bb3b3aa45e31c664917b4
Tony Messias <tony@gabstr.com>
2014-12-08 18:02:14 -0300

    adding API documentation and a few more tests

--- tests/functional/TodosTest.php
+++ tests/functional/TodosTest.php
@@ -43,4 +43,27 @@ class TodosTest extends TestCase {
         $this->assertEquals(400, $response->getStatusCode());
         $this->assertCount(0, Todo::all());
     }
+
+    /** @test */
+    function it_should_list_todos()
+    {
+        $this->createTodos(3);
+
+        $response = $this->callJSON("GET", "api/v1/todos");
+        $data = json_decode($response->getContent());
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertCount(3, $data);
+    }
+
+    /**
+     * @param $count
+     */
+    private function createTodos($count)
+    {
+        foreach (range(1, $count) as $index)
+        {
+            Todo::create(["name" => "lorem todo {$index}"]);
+        }
+    }
 }
